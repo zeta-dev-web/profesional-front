31e1c1296575b6d2a76f49cafd0d6e0bc009e537
zeta-dev <nvreparaciones@gmail.com>
2024-03-26 12:23:35 -0300

    se cambia ruta de imagenes por enlaces externos

--- src/data/CardsData.js
+++ src/data/CardsData.js
@@ -2,17 +2,17 @@ const cardsData = [
     { 
       titulo: 'Calidad Profesional', 
       contenido: 'Encontra profesionales con una amplia experiencia y conocimientos en sus áreas de especialización, garantizando la más alta calidad en cada servicio.', 
-      imagen: '../src/assets/calidad.png'
+      imagen: 'https://i.ibb.co/bd5ZyYz/calidad.png'
     },
     { 
       titulo: 'Mejor Precio', 
       contenido: 'Encontra precios competitivos y transparentes para que puedas acceder a los servicios que necesitas sin comprometer tu presupuesto.', 
-      imagen: '../src/assets/precio.png' 
+      imagen: 'https://i.ibb.co/gFYr73Q/precio.png' 
     },
     { 
       titulo: 'Seguridad Garantizada', 
       contenido: 'La seguridad es nuestra prioridad. Encontra profesionales confiables y verificados para brindarte tranquilidad y confianza.', 
-      imagen: '../src/assets/seguridad.png'
+      imagen: 'https://i.ibb.co/yNryVkC/seguridad.png'
     },
   ];
   
